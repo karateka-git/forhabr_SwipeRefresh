8f2e205363d9a26879512cac7e7007c1ffd50ca3
verofeev <zvvlad2@gmail.com>
2021-02-24 16:31:46 +0600

    VE: add dataBinding, add webView.

--- app/src/main/java/com/example/forhabr_swiperefresh/MainActivity.kt
+++ app/src/main/java/com/example/forhabr_swiperefresh/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.forhabr_swiperefresh
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.forhabr_swiperefresh.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(
+            this, R.layout.activity_main
+        )
+        binding.webView.settings.javaScriptEnabled = true
+        binding.webView.loadUrl("https://www.journaldev.com")
     }
 }
