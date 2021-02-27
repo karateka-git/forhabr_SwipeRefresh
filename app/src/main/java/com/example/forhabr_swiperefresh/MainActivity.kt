adfdb67217d67f191c5f9791072b082b6cdcc1e5
verofeev <zvvlad2@gmail.com>
2021-02-27 21:11:30 +0600

    VE: create NestedWebView (to hide the toolbar when scrolling)

--- app/src/main/java/com/example/forhabr_swiperefresh/MainActivity.kt
+++ app/src/main/java/com/example/forhabr_swiperefresh/MainActivity.kt
@@ -15,5 +15,11 @@ class MainActivity : AppCompatActivity() {
             this, R.layout.activity_main
         )
         setSupportActionBar(binding.toolbar)
+        binding.webView.settings.javaScriptEnabled = true
+        binding.webView.loadUrl("https://www.google.com/search?newwindow=1&sxsrf=ALeKk02Xt8LeDNhoH8es8g-jq_Rd0Y6MtA%3A1614334825312&ei=acs4YNKnEqKHwPAP7I2LiAo&q=disney&oq=disney&gs_lcp=Cgdnd3Mtd2l6EAxQAFgAYKwXaABwAngAgAG9AYgBvQGSAQMwLjGYAQCqAQdnd3Mtd2l6wAEB&sclient=gws-wiz&ved=0ahUKEwiSgL7uqYfvAhWiAxAIHezGAqEQ4dUDCA0")
+//        binding.swipeContainer.setOnRefreshListener {
+//            binding.webView.reload()
+//            binding.swipeContainer.isRefreshing = false
+//        }
     }
 }
