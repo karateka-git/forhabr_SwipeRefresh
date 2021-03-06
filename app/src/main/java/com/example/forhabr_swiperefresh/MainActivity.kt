633fd51d53e71edefff425fb5bcf3f2d58cc6870
verofeev <zvvlad2@gmail.com>
2021-03-06 17:08:10 +0600

    VE: add SwipeRefresh

--- app/src/main/java/com/example/forhabr_swiperefresh/MainActivity.kt
+++ app/src/main/java/com/example/forhabr_swiperefresh/MainActivity.kt
@@ -17,9 +17,9 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
         binding.webView.settings.javaScriptEnabled = true
         binding.webView.loadUrl("https://www.google.com/search?newwindow=1&sxsrf=ALeKk02Xt8LeDNhoH8es8g-jq_Rd0Y6MtA%3A1614334825312&ei=acs4YNKnEqKHwPAP7I2LiAo&q=disney&oq=disney&gs_lcp=Cgdnd3Mtd2l6EAxQAFgAYKwXaABwAngAgAG9AYgBvQGSAQMwLjGYAQCqAQdnd3Mtd2l6wAEB&sclient=gws-wiz&ved=0ahUKEwiSgL7uqYfvAhWiAxAIHezGAqEQ4dUDCA0")
-//        binding.swipeContainer.setOnRefreshListener {
-//            binding.webView.reload()
-//            binding.swipeContainer.isRefreshing = false
-//        }
+        binding.swipeContainer.setOnRefreshListener {
+            binding.webView.reload()
+            binding.swipeContainer.isRefreshing = false
+        }
     }
 }
